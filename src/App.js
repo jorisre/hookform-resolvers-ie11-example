0d459939602ba37db5e9ceb273728d8435c473da
Joris <reixjoris@gmail.com>
2020-10-10 12:27:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form/dist/index.ie11";
+import { ErrorMessage } from "@hookform/error-message";
+import { yupResolver } from "@hookform/resolvers/dist/ie11/yup";
+import * as yup from "yup";
 
-function App() {
+const schema = yup.object({
+  username: yup.string().min(3).max(30).required(),
+  email: yup.string().email(),
+  password: yup.string().matches(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  confirmPassword: yup.ref("password"),
+});
+
+const App = () => {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit((d) => alert(JSON.stringify(d)))}>
+      <label htmlFor='username'>Username :</label>
+      <input name='username' ref={register} id='username' placeholder='John' />
+      <ErrorMessage
+        errors={errors}
+        name='username'
+        render={({ message }) => <p>{message}</p>}
+      />
+
+      <label htmlFor='email'>Email address :</label>
+      <input
+        name='email'
+        ref={register}
+        id='email'
+        placeholder='johndoe@mail.com'
+      />
+      <ErrorMessage
+        errors={errors}
+        name='email'
+        render={({ message }) => <p>{message}</p>}
+      />
+
+      <label htmlFor='password'>Password :</label>
+      <input name='password' ref={register} id='password' type='password' />
+      <ErrorMessage
+        errors={errors}
+        name='password'
+        render={({ message }) => <p>{message}</p>}
+      />
+
+      <label htmlFor='confirmPassword'>Confirm password :</label>
+      <input
+        name='confirmPassword'
+        ref={register}
+        id='confirmPassword'
+        type='password'
+      />
+      <ErrorMessage
+        errors={errors}
+        name='confirmPassword'
+        render={({ message }) => <p>{message}</p>}
+      />
+      <input type='submit' />
+    </form>
   );
-}
+};
 
 export default App;
